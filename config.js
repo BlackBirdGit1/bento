68d4c53caa2c220a5c28e4e6b809627f093e2c88
BlackBirdGit1 <thinkgallery1@gmail.com>
2025-07-05 18:41:10 -0400

    Update config.js

--- config.js
+++ config.js
@@ -26,14 +26,14 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+	weatherKey: '5efafb1ab4d3edd94b1b2fc166ab650c', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
-	weatherUnit: 'C', // 'F', 'C'
+	weatherUnit: 'F', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
-	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
+	trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
+	defaultLatitude: '32.0809',
+	defaultLongitude: '-81.0912',
 
 	// Autochange
 	autoChangeTheme: true,
